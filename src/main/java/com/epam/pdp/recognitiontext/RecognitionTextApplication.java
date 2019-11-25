fe463a1c67dcb6b56b48db42fb0a8ab65c01f56e
p0keT <p0keT@ukr.net>
2019-11-25 22:40:36 +0200

    Initial commit

---
+++ src/main/java/com/epam/pdp/recognitiontext/RecognitionTextApplication.java
@@ -0,0 +1,13 @@
+package com.epam.pdp.recognitiontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecognitionTextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecognitionTextApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/pdp/recognitiontext/RecognitionTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.pdp.recognitiontext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecognitionTextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
